cf83bda16d9648e69821fb05cae1dccb2097206a
jaehyeon Ye <forthevision7@gmail.com>
2022-02-09 18:12:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ const gamerestarter = document.querySelector("#Gamerestart");
 const showrandomnumber = document.querySelector("#Shownumber");
 const numberInput = document.querySelector("#InputNumber");
 const getputNumber = document.querySelector("#InputNumber input");
-//const showStrikeResult = document.querySelector(".ShowResults");
-//const showBallResult = document.querySelector(".ShowResults");
+const showChanceResult = document.querySelector("#ShowResults span:first-child");
+const showStrikeResult = document.querySelector("#ShowResults span:nth-child(2)");
+const showBallResult = document.querySelector("#ShowResults span:nth-child(3)");
+const showOutResult = document.querySelector("#ShowResults span:last-child");
 const RandomNumber1 = Math.floor(Math.random()*9 +1);
 const RandomNumber2 = Math.floor(Math.random()*10);
 const RandomNumber3 = Math.floor(Math.random()*10);
-//let tryinputNumber = [];
+//let sum = 0;
+let countChance = 10;
+let OutCount = 0;
 //const newNumber = parseInt(getputNumber.value);
 
 gamestarter.addEventListener("click",makeRandomNumber);
@@ -16,9 +20,6 @@ gamerestarter.addEventListener("click",refreshfunction);
 
 function makeRandomNumber(event){
     event.preventDefault();
-    //const RandomNumber1 = Math.floor(Math.random()*9 +1);
-    //const RandomNumber2 = Math.floor(Math.random()*10);
-    //const RandomNumber3 = Math.floor(Math.random()*10);
     showrandomnumber.innerText = `${RandomNumber1}${RandomNumber2}${RandomNumber3}`
     return RandomNumber1, RandomNumber2, RandomNumber3;
 }
@@ -28,11 +29,14 @@ numberInput.addEventListener("submit",confirmInputNumber);
 function confirmInputNumber(event){
     event.preventDefault();
     const newNumber = parseInt(getputNumber.value);
-    //console.log(newNumber, typeof(newNumber));
     getinputNumber(newNumber);
-    //console.log("Put Three figure number!!");
+    countChance-=1;
+    if(countChance===-1){
+        alert("Game Over!");
+        location.reload();
+    }
 }
-//makeRandomNumbers();
+
 function getinputNumber(newNumber){
     if(newNumber<100||newNumber>=1000){
         alert("Put Three figures!!");
@@ -45,49 +49,67 @@ function compareNumbers(RandomNumber1, RandomNumber2, RandomNumber3, newNumber){
     const RN1 = RandomNumber1.toString(10);
     const RN2 = RandomNumber2.toString(10);
     const RN3 = RandomNumber3.toString(10);
-    const RNresult = RN1+RN2+RN3;
+    const RNary = [RN1, RN2, RN3];
+    //const RNresult = RN1+RN2+RN3;
     const INum = newNumber.toString(10);
     let countStrike = 0;
     let countBall = 0;
-    let countChance = 10;
-    let flag = 0;
-    //console.log(RN1, RN2, RN3, typeof(RN1),typeof(RN2), typeof(RN3));
-    //console.log(INum, typeof(INum));
-    //console.log(INum.indexOf(RN1));
-    //console.log(INum.indexOf(RN2));
-    //console.log(INum.indexOf(RN3));
-    //console.log(RNresult, typeof(RNresult));
-    
+    //let Ballmark = 0;
+    //let flag = 0;
+    let slicedINum = "";
+    let slicedINumary = [];
+    //let filteredRNary = "";
+    let numberofsameElement = 0;
+    let sum = 0;
+    //let finalary = sortedslicedINumary-sortedRNary;
+
     for(let i=0;i<INum.length;i++){
-        let slicedINum = INum.slice(i,i+1);
-        //console.log(slicedINum);
-        if(slicedINum===RN1){
-            if(flag!==0){
-                countBall+=1;
-            }else{
-                countStrike+=1;
-            }
-        }else if(slicedINum===RN2){
-            if(flag!==1){
-                countBall+=1;
-            }else{
-                countStrike+=1;
-            }
-        }else if(slicedINum===RN3){
-            if(flag!==2){
-                countBall+=1;
-            }else{
-                countStrike+=1;
+        slicedINum = INum.slice(i,i+1);
+        slicedINumary.push(slicedINum);
+        //sortedslicedINumary = slicedINumary.sort();
+        const filteredRNary = RNary.filter(x=>{return x === slicedINum});
+        //numberofsameElement = filteredRNary.length;
+        sum = sum + filteredRNary.length;
+        console.log(filteredRNary, filteredRNary.length, sum);
+
+        for(let j=0;j<INum.length;j++){
+            //let slicedINum = INum.slice(j,j+1);
+            if(slicedINumary[j]===RNary[i]){
+                if(j===i){
+                    countStrike+=1;
+                }else{
+                    countBall+=1;
+                    countBall+=sum;
+                }
             }
         }
-        flag+=1;
+        //flag+=1;
+        
+        if(countStrike===3){
+            alert("Congrats! You Win!");
+            OutCount+=1;
+            OutCounting(OutCount);
+            countStrike=0;
+            countChance=10;
+            //location.reload();
+        }    //console.log("OutCount : ", OutCount);
     }
-    console.log("countStrike : ", countStrike);
-    console.log("countBall : ", countBall);
-    console.log("countChance : ", countChance);
-    console.log("flag : ",flag);
+    
+    showChanceResult.innerText = `Chance : ${countChance}\n`
+    showStrikeResult.innerText = `S : ${countStrike}\n`
+    showBallResult.innerText = `B : ${countBall}\n`
+    //showOutResult.innerText = `O : ${OutCount}`
+    //console.log("countChance : ", countChance);
+    //console.log(filteredRNary);
 }
+
 
 function refreshfunction(){
     location.reload();
 }
+//showOutResult.innerText = `O : ${OutCount}`
+function OutCounting(OutCount){
+    showOutResult.innerText = `O : ${OutCount}`
+}
+
+
